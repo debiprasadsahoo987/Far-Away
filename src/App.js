4fe44a5d2659bee2c26fe55dd1463ec2a957b6a6
Debiprasad <debiprasadsahoo987@gmail.com>
2024-11-23 19:56:38 +0530

    added forms

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@
 // import Form from "./Components/Form";
 // import PackingList from "./Components/PackingList";
 // import Stats from "./Components/Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 export default function App() {
   return (
     <div className="app">
@@ -18,15 +24,45 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit() {}
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you want for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul style={{ overflow: "hidden" }}>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
